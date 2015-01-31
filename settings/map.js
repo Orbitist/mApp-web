4038765de24f16c8d9ea8a81aea6246ef943d1e2
ngunner <gunnernr@gmail.com>
2015-01-31 08:54:46 -0500

    initual upload

---
+++ settings/map.js
@@ -0,0 +1,18 @@
+ var map = new L.Map('map', {
+	center: [0, 0],
+	zoom: 3
+});
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/orbitist.ie90k9cc/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://mapbox.com">Mapbox</a>'
+}).addTo(map);
+// use cartodb layer
+cartodb.createLayer(map, 'http://orbitist.cartodb.com/api/v2/viz/040ba3cc-0252-11e4-ae4c-0e230854a1cb/viz.json')
+	.addTo(map)
+	.on('done', function(layer) {
+		//do stuff
+	})
+	.on('error', function(err) {
+	alert("some error occurred: " + err);
+	});
