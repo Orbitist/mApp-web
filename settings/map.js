99153e29473c86406103d928ad3b6d5ac84b2cda
ngunner <gunnernr@gmail.com>
2015-01-31 09:13:47 -0500

    test without carted

--- settings/map.js
+++ settings/map.js
@@ -7,12 +7,13 @@
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/orbitist.jeijd89i/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://mapbox.com">Mapbox</a>'
 }).addTo(map);
+
 // use cartodb layer
-cartodb.createLayer(map, 'http://orbitist.cartodb.com/api/v2/viz/040ba3cc-0252-11e4-ae4c-0e230854a1cb/viz.json')
-	.addTo(map)
-	.on('done', function(layer) {
-		//do stuff
-	})
-	.on('error', function(err) {
-	alert("some error occurred: " + err);
-	});
+// cartodb.createLayer(map, 'http://orbitist.cartodb.com/api/v2/viz/040ba3cc-0252-11e4-ae4c-0e230854a1cb/viz.json')
+// 	.addTo(map)
+// 	.on('done', function(layer) {
+// 		//do stuff
+// 	})
+// 	.on('error', function(err) {
+// 	alert("some error occurred: " + err);
+// });
