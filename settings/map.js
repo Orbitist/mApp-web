88e7b3f0d25142cbc92f8eb7111cea65c3d79e3e
ngunner <gunnernr@gmail.com>
2015-01-31 09:09:16 -0500

    Different map

--- settings/map.js
+++ settings/map.js
@@ -4,7 +4,7 @@
 });
 
 // add an OpenStreetMap tile layer
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/orbitist.l3d32kbn/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/orbitist.jeijd89i/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://mapbox.com">Mapbox</a>'
 }).addTo(map);
 // use cartodb layer
