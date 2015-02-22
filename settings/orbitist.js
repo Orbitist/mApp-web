d949b4feef15a516c13141f4dd874c6b8cda2508
ngunner <gunnernr@gmail.com>
2015-02-22 10:48:12 -0500

    binoculars

--- settings/orbitist.js
+++ settings/orbitist.js
@@ -3,8 +3,8 @@ var orbitistApiGeoJson = {"type":"FeatureCollection","features":[{"type":"Featur
   
 // Markers are configured here:
 var orbitistIcon = L.divIcon({
-        className: 'fa-binoculars',
-        html: '<i class="fa fa-bus"></i>',
+        className: 'fa-icon',
+        html: '<i class="fa fa-binoculars"></i>',
         iconSize: [20, 20]
     });
   
