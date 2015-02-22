9c1a3ebb9294f5396a2b426ddc3aa1478b564c99
ngunner <gunnernr@gmail.com>
2015-02-22 11:27:57 -0500

    no style

--- settings/orbitist.js
+++ settings/orbitist.js
@@ -4,7 +4,8 @@ var orbitistApiGeoJson = {"type":"FeatureCollection","features":[{"type":"Featur
 // Markers are configured here:
 var orbitistIcon = L.divIcon({
         className: 'fa-icon', // Don't change this unless you're really a genius
-        html: '<i class="fa fa-map-marker"></i>'
+        html: '<i class="fa fa-map-marker"></i>',
+        iconSize: [35, 35]
     });
   
 // ADVANCED: This tells the map what goes in popups.
